b656810bb5e56a45b246531e60033d9a3aa31d8b
Tammarra McPhaul <shaemc22@gmail.com>
2022-01-24 19:16:56 -0500

    commit tree 99bbe5596ba237e30e85558e3086325b4775057f

--- main.js
+++ main.js
@@ -4,8 +4,32 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const likesContainer = document.querySelector('li')
+const allHearts = document.querySelectorAll('.like-glyph')
 
+for (const hrt of allHearts) {
+  hrt.addEventListener('click', addLike)
+}
 
+function addLike(e) {
+  if (e.target.innerText === EMPTY_HEART) {
+    mimicServerCall()
+    .then(() => {
+      e.target.innerText = FULL_HEART; 
+      e.target.classList.toggle('activated-heart');  
+    })
+    .catch(err => {
+      const modal = document.getElementById("modal");
+      modal.classList.toggle('hidden'); 
+      modal.innerText = err;
+      setTimeout(() => modal.classList.toggle('hidden'), 3000);
+    })
+  }
+  if (e.target.innerText === FULL_HEART) {
+    e.target.innerText = EMPTY_HEART; 
+    e.target.classList.toggle('activated-heart'); 
+  }
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
